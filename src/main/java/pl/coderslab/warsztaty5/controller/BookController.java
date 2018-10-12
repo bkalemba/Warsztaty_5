3f12caaa5f2e16bb70680d10c77e57a048694a7f
Beniamin Kalemba <kalembabeniamin@gmail.com>
2018-10-12 14:01:25 +0200

    Implement show book list functionality

--- src/main/java/pl/coderslab/warsztaty5/controller/BookController.java
+++ src/main/java/pl/coderslab/warsztaty5/controller/BookController.java
@@ -1,13 +1,20 @@
 package pl.coderslab.warsztaty5.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.coderslab.warsztaty5.beans.MemoryBookService;
 import pl.coderslab.warsztaty5.model.Book;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
+
+    @Autowired
+    private MemoryBookService memoryBookService;
 
     @GetMapping("/hello")
     public String hello(){
@@ -18,5 +25,10 @@ public class BookController {
     public Book helloBook(){
         return new Book(1L,"9788324631766","Thinking in Java",
                 "Bruce Eckel","Helion","programming");
+    }
+
+    @GetMapping("")
+    public List<Book> getBookList(){
+        return memoryBookService.getList();
     }
 }
